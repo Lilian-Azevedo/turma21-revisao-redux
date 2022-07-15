602b4b4bc3adc2c7be470f6134bd983e6cce8252
Lilian Azevedo <lilian.azevedo0905@gmail.com>
2022-07-15 16:48:34 -0300

    :sparkles: Feat: add Form.js

---
+++ src/components/Form.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import { setUserInfos } from '../redux/actions';
+
+import '../styles/styles.css';
+
+const INITIAL_STATE = { name: '', height: 0, weight: 0, imc: 0};
+
+class Form extends Component {
+  state = INITIAL_STATE;
+ 
+  handleChange = ({ target: { name, value } }) => {
+    this.setState({ [name]: value });
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    const { height, weight } = this.state;
+    const multiply = parseFloat(height) * parseFloat(height);
+    const imcCalc = parseFloat(weight)/multiply;
+
+    this.setState((prevInfo) => ({ ...prevInfo, imc: imcCalc}),
+      () => this.handleDispatch());
+  }
+
+  handleDispatch = () => {
+    const { userDispatch } = this.props;
+    userDispatch(this.state);
+    this.setState(INITIAL_STATE);
+  }
+
+  render() {
+    const { name, height, weight } = this.state;
+
+    return (
+      <form onSubmit={ this.handleSubmit } className='form'>
+        <input
+          name="name"
+          type="text"
+          value={ name }
+          onChange={ this.handleChange }
+          placeholder='NOME'
+        />
+        <input
+          name="height"
+          type="decimal"
+          value={ height }
+          onChange={ this.handleChange }
+          placeholder='ALTURA'
+        />
+        <input
+          name="weight"
+          type="decimal"
+          value={ weight }
+          onChange={ this.handleChange }
+          placeholder='PESO'
+        />
+        <button type="submit">
+          Submit
+        </button>
+      </form>
+    );
+  }
+}
+
+const mapDispatchToProps = (dispatch) => ({
+  userDispatch: (user) => dispatch(setUserInfos(user)),
+});
+
+export default connect(null, mapDispatchToProps)(Form);
